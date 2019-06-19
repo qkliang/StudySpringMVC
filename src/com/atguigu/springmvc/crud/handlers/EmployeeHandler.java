d024c92f08d56ffe064de72d14ea541d274acb5a
lqk <qkliang@foxmail.com>
2019-06-19 22:55:18 +0800

    1

---
+++ src/com/atguigu/springmvc/crud/handlers/EmployeeHandler.java
@@ -0,0 +1,25 @@
+package com.atguigu.springmvc.crud.handlers;
+
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.atguigu.springmvc.crud.dao.DepartmentDao;
+import com.atguigu.springmvc.crud.dao.EmployeeDao;
+import com.atguigu.springmvc.crud.entities.Employee;
+
+@Controller
+public class EmployeeHandler {
+
+	
+}
